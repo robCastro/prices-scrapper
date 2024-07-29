222ac6c42785914fade9c0bcb6d56bbffcc67901
Roberto Castro <roberto.ernesto.castro@gmail.com>
2024-07-29 10:21:51 -0600

    Added relativa path to data.csv

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ from pathlib import Path
 
 def main():
     print('Starting')
-    productos = load_products()
     now = datetime.now()
-    with open('data.csv', 'a') as file:
+    current_path = Path(__file__).parent
+    productos = load_products(current_path)
+    filename = current_path.joinpath('data.csv')
+    with open(filename, 'a') as file:
         for producto in productos:
             precio = producto.get_precio()
             file.write(f'{producto.descripcion}, {precio}, {now:%d/%m/%Y %H:%M:%S} \n')
     print('Done')
 
-def load_products():
+def load_products(current_path: Path):
     productos = []
-    filename = Path(__file__).parent.joinpath('productos.json')
+    filename = current_path.joinpath('productos.json')
     with open(filename, 'r') as file_content:
         json_dictionary = json.load(file_content)
         productos = [Producto(**data) for data in json_dictionary]
