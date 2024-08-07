28ce2585b7ad23e53e27e6600ac664ae9c7a5d86
Roberto Castro <roberto.ernesto.castro@gmail.com>
2024-08-07 15:52:09 -0600

    Migrating to django

---
+++ prices_scrapper/scrapper/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+class Product(models.Model):
+    description = models.CharField(max_length=500)
+    url = models.CharField(max_length=200)
+    selector = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.description
+    
+    def get_price(self) -> float:
+        html = urlopen(url=self.url).read().decode('utf-8')
+        soup = BeautifulSoup(html, 'html.parser')
+        price_string = soup.select_one(self.selector).get_text().replace(',', '.').replace('$', '')
+        return float(price_string)
+
+
+class PriceObservation(models.Model):
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.product}, ${self.price}, {self.created_at:%d/%m/%Y %H:%M:%S}'
+
