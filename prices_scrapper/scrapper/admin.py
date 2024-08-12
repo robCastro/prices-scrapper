f803ba48707d11bc3cdfb809b977c4339f19dd09
Roberto Castro <roberto.ernesto.castro@gmail.com>
2024-08-12 09:16:09 -0600

    Added filters and display fields

--- prices_scrapper/scrapper/admin.py
+++ prices_scrapper/scrapper/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Product, PriceObservation
 
-admin.site.register(Product)
-admin.site.register(PriceObservation)
+
+class PriceObservationAdmin(admin.ModelAdmin):
+    list_filter = ['product', 'product__user', 'created_at']
+    list_display = ['product', 'price', 'created_at']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['description', 'user']
+    list_filter = ['user']
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(PriceObservation, PriceObservationAdmin)
